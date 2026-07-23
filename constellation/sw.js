3793d5632bd286c396c9dc831bd0979c9f841c6a
sahilkanetkar-collab <sahil.kanetkar@gmail.com>
2026-07-23 23:06:51 +0530

    Add Formula Constellation PWA: sw.js

---
+++ constellation/sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'fc-v1';
+const SHELL = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/katex.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/katex.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  const cacheable =
+    url.includes('/constellation/') ||
+    url.includes('cdnjs.cloudflare.com/ajax/libs/KaTeX') ||
+    url.includes('fonts.googleapis.com') ||
+    url.includes('fonts.gstatic.com');
+  if (!cacheable || e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      if (hit) return hit;
+      return fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
